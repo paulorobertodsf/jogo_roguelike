4c8f74d3dc5883dd2341dcb2b1f6e4b1292b9e50
Leticia <leticia-olive@hotmail.com>
2025-06-03 21:08:43 -0300

    animations

---
+++ Assets/Script/PlayerAnimation.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    [SerializeField] private Rigidbody2D rigidBody;
+
+    [SerializeField] private SpriteRenderer sprite;
+
+
+    void start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+    void Update()
+    {
+        float velocidadeX = this.rigidBody.linearVelocity.x;
+        float velocidadeY = this.rigidBody.linearVelocity.y;
+
+        if (velocidadeX > 0)
+        {
+            this.animator.SetBool("run", true);
+            this.animator.SetBool("right", true);
+            this.animator.SetBool("left", false);
+            sprite.flipX = false;
+        }else if(velocidadeX < 0)
+        {
+            this.animator.SetBool("run", true);
+            this.animator.SetBool("right", false);
+            this.animator.SetBool("left", true);
+            sprite.flipX = true;
+        }else if (velocidadeY < 0  && velocidadeX == 0)
+        {
+            this.animator.SetBool("run", true);
+            this.animator.SetBool("right", false);
+            this.animator.SetBool("left", false);
+        }else if (velocidadeY > 0 && velocidadeX == 0)
+        {
+            this.animator.SetBool("run", true);
+            this.animator.SetBool("right", false);
+            this.animator.SetBool("left", false);
+        }
+        else
+        {
+            this.animator.SetBool("run", false);
+            this.animator.SetBool("right", false);
+            this.animator.SetBool("left", false);   
+        }
+    }
+
+}
